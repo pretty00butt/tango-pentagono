aea8e66952cb55ea14dd93d4df2dcd2024bacd15
hoony <hoonyland.newsletter@gmail.com>
2022-08-28 09:39:32 +0200

    feat: deploay

--- next.config.js
+++ next.config.js
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
+  assetPrefix: isProd ? "/tango-pentagono/" : "",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
