db8b07ed84bc9098a2b6f3e326ddf69114e9d00b
hoony <hoonyland.newsletter@gmail.com>
2022-08-28 10:39:51 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
-const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  basePath: "/tango-pentagono",
-  assetPrefix: "/tango-pentagono/",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
