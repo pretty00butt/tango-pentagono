7e6a7fe5f76af893beee9dc1b0fa644837ddee86
hoony <hoonyland.newsletter@gmail.com>
2022-08-28 10:27:00 +0200

    fix

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === "production";
 
 module.exports = {
-  assetPrefix: isProd ? "/tango-pentagono/" : "",
+  assetPrefix: "/tango-pentagono/",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
